b219cd661c5cb0cb208d271bf7dc5f878d48d560
SamKon6 <44882800+SamKon6@users.noreply.github.com>
2018-11-15 22:35:00 -0600

    Add files via upload

--- guided_search.php
+++ guided_search.php
@@ -14,13 +14,13 @@ echo "<br>";
 <?php
 
 $name = $_GET['name'];
-$number = $_GET['number'];
+$section = $_GET['number'];
 $level = $_GET['level'];
 $location = $_GET['location'];
 
 $user = 'root';
 $password = 'root';
-$db = 'courses';
+$db = 'course_registration';
 $host = 'localhost';
 $port = 3306;
 
@@ -38,20 +38,23 @@ if (!$conn){
 	exit;
 }
 
-$sql = "SELECT * FROM classes WHERE ";
+$sql = "SELECT * FROM course c
+        INNER JOIN course_professor cp ON c.CID = cp.CID
+        INNER JOIN professor p ON p.PID = cp.PID
+        WHERE ";
 
 if($name != "")
 {
-  $sql .= "name like '%" . $name . "%' AND ";
+  $sql .= "Cname like '%" . $name . "%' AND ";
 }
 else
 {
   $sql .= "1 AND ";
 }
 
-if($number != "")
+if($section != "")
 {
-  $sql .= "number = " . $number . " AND ";
+  $sql .= "Section like '%" . $section . "%' AND ";
 }
 else
 {
@@ -67,26 +70,29 @@ else
   $sql .= "1 AND ";
 }
 
-if($location != "any")
+if($location == "any")
 {
-  $sql .= "location = '" . $location . "'";
-  echo $location;
+  $sql .= "1";
+}
+else if($location == 'online')
+{
+  $sql .= "Location = '" . $location . "'";
 }
 else
 {
-  $sql .= "1";
+  $sql .= "Location != 'online'";
 }
 
 $sql .= " order by section asc";
 
 $result = mysqli_query($conn, $sql);
 
-echo "<table class='table table-striped'><tr><td>Section</td><td>Class Number</td><td>Class Title</td><td>Instructor</td><td>Time</td><td>Location</td><td>Fill</td><td>Add To Favorites</td></tr>";
+echo "<table class='table table-striped'><tr><td>Class Section</td><td>Class Name</td><td>Professor</td><td>Time</td><td>Location</td><td>Fill</td><td>Add To Favorites</td></tr>";
 
 while($row = mysqli_fetch_array($result))
 {
-	echo "<tr><td>". $row["section"] . "</td><td>". $row["number"]. "</td><td>". $row["name"]. "</td><td>". $row["professor"]. "</td><td>". $row["time"]. "</td><td>". $row["location"]. "</td>";
-  if($row["open_seats"]==0)
+	echo "<tr><td>". $row["Section"] . "</td><td>". $row["CName"].  "</td><td>". $row["FName"]. " ". $row["LName"] . "</td><td>". $row["Schedule"]. "</td><td>". $row["Location"]. "</td>";
+  if($row["OpenSeats"]==0)
   {
     echo "<td>FULL</td>";
   }
@@ -94,8 +100,10 @@ while($row = mysqli_fetch_array($result))
   {
     echo "<td>OPEN</td>";
   }
-  $link = 'http://localhost:8888/favorites.php?number=' . $row["number"];
-  echo "<td><a href=" . $link . ">Add To Favorites</a></td></tr>";
+
+  echo "<td> <button onclick='add_to_favorites(" . $row["CID"] . ")'>Add</button> </td></tr>";
+
+
 }
 
 echo "</table>";
