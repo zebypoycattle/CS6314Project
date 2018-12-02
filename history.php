fcb862ff58557213e4e959386daf3fbc1e546f98
zebypoycattle <zeby.poycattle@utexas.edu>
2018-12-02 00:05:36 -0600

    Add files via upload

--- history.php
+++ history.php
@@ -69,10 +69,10 @@
 
     $result = mysqli_query($conn, $sql);
 
-    echo "<table class='table table-striped'><tr><td>Department</td><td>Course Number</td><td>Section</td><td>Course Name</td><td>Semester</td><td>Year</td><td>Day</td><td>Time</td><td>Location</td><td>Level</td></tr>";
+    echo "<table class='table table-striped'><tr><td>Department</td><td>Course Number</td><td>Section</td><td>Course Name</td><td>Semester</td><td>Year</td><td>Schedule<td>Location</td><td>Level</td></tr>";
   
     while($row = mysqli_fetch_array($result)) {
-      echo "<tr><td>".$row["DName"]."</td><td>".$row["CNumber"]."</td><td>" .$row["Section"]. "</td><td>". $row["CName"]."</td><td>".$row["Semester"]."</td><td>".$row["Year"]."</td><td>".$row["Day"]."</td><td>".$row["Time"]."</td><td>".$row["Location"]."</td><td>".$row["Level"]."</td></tr>";
+      echo "<tr><td>".$row["DName"]."</td><td>".$row["CNumber"]."</td><td>" .$row["Section"]. "</td><td>". $row["CName"]."</td><td>".$row["Semester"]."</td><td>".$row["Year"]."</td><td>".$row["Day"]." ".$row["Time"]."</td><td>".$row["Location"]."</td><td>".$row["Level"]."</td></tr>";
     }
   }
   else {
