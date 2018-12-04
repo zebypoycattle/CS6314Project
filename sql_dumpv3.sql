92e78a443a96a82d647a851ceb0e4d7932056ee1
Tao Wang <Feynman@Taos-MacBook-Pro.local>
2018-12-03 22:07:36 -0600

    change sql_dumpv3.sql.txt to sql_dumpv3.sql

---
+++ sql_dumpv3.sql
@@ -0,0 +1,146 @@
+CREATE TABLE `course_registration`.`Department` ( `DID` VARCHAR(10) NOT NULL, `DName` TEXT NOT NULL, PRIMARY KEY (`DID`)) ENGINE = InnoDB;
+INSERT INTO `Department` (`DID`, `Dname`) VALUES ('1', 'Computer Science');
+INSERT INTO `Department` (`DID`, `Dname`) VALUES ('2', 'Software Engineering');
+
+CREATE TABLE `course_registration`.`Course` ( `CID` INT NOT NULL AUTO_INCREMENT , `DID` TEXT NOT NULL, `CNumber` TEXT NOT NULL, `Section` TEXT NOT NULL , `CName` TEXT NOT NULL , `Semester` TEXT NOT NULL , `Year` VARCHAR(4) NOT NULL, `Day` TEXT NOT NULL, `Time` TEXT NOT NULL, `Location` TEXT NOT NULL , `Quota` INT NOT NULL , `Level` TEXT NOT NULL , `isDeleted` BOOLEAN NOT NULL, `EnrolledSeats` INT NOT NULL, PRIMARY KEY (`CID`)) ENGINE = InnoDB;
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '4341', '001', 'Digital Logic', 'Spring', 2018, 'M/W', '1:00pm-2:15pm', 'CB 1.120', '15', 'Undergraduate', '0', '15');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '4347', '001', 'Database Systems', 'Spring', 2018, 'M/W', '2:30pm-3:45pm', 'ECSN 2.120', '44', 'Undergraduate', '0', '42');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6314', '001', 'Web Programming Languages', 'Fall', 2018, 'T/TH', '8:30am-9:45am', 'ECSS 2.410', '80', 'Graduate', '0', '70');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6375', '003', 'Machine Learning', 'Fall', 2018, 'T/TH', '1:00pm-2:15pm', 'ECSS 2.410', '70', 'Graduate', '0', '50');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6301', '004', 'Language Based Security', 'Fall', 2018, 'M/W', '1:00pm-2:15pm', 'CB 1.120', '15', 'Graduate', '0', '15');
+
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '4348', '001', 'Operating System Concepts', 'Spring', 2019, 'M/W', '1:00pm-2:15pm', 'ECSN 2.120', '50', 'Undergraduate', '0', '45');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '4348', '002', 'Operating System Concepts', 'Spring', 2019, 'M/W', '2:30pm-3:45pm', 'ECSN 2.120', '50', 'Undergraduate', '0', '50');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6314', '001', 'Web Programming Languages', 'Spring', 2019, 'M/W', '8:30am-9:45am', 'ECSS 2.410', '80', 'Graduate', '0', '70');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6314', '002', 'Web Programming Languages', 'Spring', 2019, 'T/TH', '10:00am-11:15am', 'ECSS 2.410', '80', 'Graduate', '0', '78');
+
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6375', '001', 'Machine Learning', 'Spring', 2019, 'M/W', '1:00pm-2:15pm', 'ECSS 2.202', '70', 'Graduate', '0', '66');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6375', '002', 'Machine Learning', 'Spring', 2019, 'T/TH', '1:00pm-2:15pm', 'Online', '70', 'Graduate', '0', '60');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`)  VALUES ('1', '6375', '003', 'Machine Learning', 'Spring', 2019, 'T/TH', '2:30pm-3:45pm', 'ECSS 2.202', '70', 'Graduate', '0', '70');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6313', '001', 'Statistical Methods', 'Spring', 2019, 'M/W', '10:00am-11:15pm', 'Online', '20', 'Graduate', '0', '10');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6313', '002', 'Statistical Methods', 'Spring', 2019, 'T/TH', '1:00pm-2:15pm', 'ECSN 2.122', '20', 'Graduate', '0', '20');
+
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6364', '001', 'Artificial Intelligence', 'Spring', 2019, 'M/W', '1:00pm-2:15pm', 'ECSS 2.204', '15', 'Graduate', '0', '15');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6364', '002', 'Artificial Intelligence', 'Spring', 2019, 'T/TH', '1:00pm-2:15pm', 'ECSS 2.210', '15', 'Graduate', '0', '12');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6360', '001', 'Database Design', 'Spring', 2019, 'M/W', '8:30am-9:45am', 'ECSN 2.204', '40', 'Graduate', '0', '22');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6360', '002', 'Database Design', 'Spring', 2019, 'T/TH', '8:30am-9:45am', 'ECSN 2.210', '40', 'Graduate', '0', '32');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6361', '001', 'Advanced Algorithms', 'Spring', 2019, 'M/W', '2:30pm-3:45pm', 'ECSN 2.222', '40', 'Graduate', '0', '40');
+
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6361', '002', 'Advanced Algorithms', 'Spring', 2019, 'T/TH', '2:30pm-3:45pm', 'ECSN 2.310', '40', 'Graduate', '0', '32');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6361', '003', 'Advanced Algorithms', 'Spring', 2019, 'M/W', '10:00am-11:45am', 'ECSN 2.200', '40', 'Graduate', '0', '20');
+INSERT INTO `Course` (`DID`, `CNumber`, `Section`, `CName`, `Semester`, `Year`, `Day`, `Time`, `Location`, `Quota`, `Level`, `isDeleted`, `EnrolledSeats`) VALUES ('1', '6361', '004', 'Advanced Algorithms', 'Spring', 2019, 'T/TH', '8:30am-9:45am', 'ECSN 3.210', '40', 'Graduate', '0', '40');
+
+CREATE TABLE `course_registration`.`Professor` ( `PID` INT NOT NULL AUTO_INCREMENT , `DID` TEXT NOT NULL, `FName` TEXT NOT NULL , `LName` TEXT NOT NULL , `Email` TEXT NOT NULL , PRIMARY KEY (`PID`)) ENGINE = InnoDB;
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Eric', 'Becker', 'erikbecker@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Nurcan', 'Yuruk', 'nurcanyuruk@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Chris', 'Davis', 'chrisdavis@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Anurag', 'Nagar', 'anuragnagar@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Greg', 'Ozbirn', 'gregozbirn@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Min', 'Chen', 'minchen@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Anjum', 'Chida', 'anjumchida@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Dan', 'Moldovan', 'danmoldovan@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Richard', 'Min', 'anjumchida@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Kyle', 'Fox', 'kylefox@utdallas.edu');
+INSERT INTO `Professor` (`DID`, `FName`, `LName`, `Email`) VALUES ('1', 'Sergey', 'Bereg', 'anjumchida@utdallas.edu');
+
+CREATE TABLE `course_registration`.`Student_Course` ( `SID` INT NOT NULL , `CID` INT NOT NULL , PRIMARY KEY (`SID`, `CID`));
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('1', '1');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('1', '2');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('1', '3');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('1', '8');
+
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('2', '1');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('2', '2');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('2', '3');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('2', '9');
+
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('3', '1');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('3', '2');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('3', '3');
+INSERT INTO `Student_Course` (`SID`, `CID`) VALUES ('3', '10');
+
+
+CREATE TABLE `course_registration`.`Course_Professor` ( `CID` INT NOT NULL , `PID` INT NOT NULL , PRIMARY KEY (`CID`, `PID`));
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('1', '1');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('2', '1');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('3', '2');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('4', '4');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('5', '3');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('6', '5');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('7', '5');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('8', '2');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('9', '2');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('10', '7');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('11', '7');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('12', '4');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('13', '6');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('14', '6');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('15', '8');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('16', '8');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('17', '9');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('18', '3');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('19', '3');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('20', '11');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('21', '11');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('22', '12');
+INSERT INTO `Course_Professor` (`CID`, `PID`) VALUES ('23', '12');
+
+CREATE TABLE `course_registration`.`Favorites` ( `SID` INT NOT NULL , `CID` INT NOT NULL , PRIMARY KEY (`SID`, `CID`));
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('1', '6');
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('1', '7');
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('2', '12');
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('2', '13');
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('3', '8');
+INSERT INTO `Favorites` (`SID`, `CID`) VALUES ('3', '9');
+
+CREATE TABLE `course_registration`.`Cart` ( `SID` INT NOT NULL , `CID` INT NOT NULL , PRIMARY KEY (`SID`, `CID`));
+INSERT INTO `Cart` (`SID`, `CID`) VALUES ('1', '12');
+INSERT INTO `Cart` (`SID`, `CID`) VALUES ('2', '13');
+INSERT INTO `Cart` (`SID`, `CID`) VALUES ('3', '14');
+
+CREATE TABLE `course_registration`.`User` ( `Username` VARCHAR(10) NOT NULL , `Category` TEXT NOT NULL , `Pwd` TEXT NOT NULL , `FName` TEXT NOT NULL , `LName` TEXT NOT NULL , `Email` TEXT NOT NULL, PRIMARY KEY (`Username`));
+INSERT INTO `User` (`Username`, `Category`, `Pwd`, `Fname`, `LName`, `Email`) VALUES ('jxw123', 'student', '$2y$10$QZjK/Hj2CAemuNE3KDKoD.2HqTA9nMjnJj3PnQexFO7Y/7hqeJhuK', 'John', 'Will', 'jw@gmail.com');
+INSERT INTO `User` (`Username`, `Category`, `Pwd`, `Fname`, `LName`, `Email`) VALUES ('mxh789', 'student', '$2y$10$QZjK/Hj2CAemuNE3KDKoD.2HqTA9nMjnJj3PnQexFO7Y/7hqeJhuK', 'Ming', 'Hao', 'mh@gmail.com');
+INSERT INTO `User` (`Username`, `Category`, `Pwd`, `Fname`, `LName`, `Email`) VALUES ('zxp000', 'admin', '$2y$10$QZjK/Hj2CAemuNE3KDKoD.2HqTA9nMjnJj3PnQexFO7Y/7hqeJhuK', 'Zeby', 'Poycattle', 'zp@gmail.com');
+INSERT INTO `User` (`Username`, `Category`, `Pwd`, `Fname`, `LName`, `Email`) VALUES ('rxg456', 'student', '$2y$10$QZjK/Hj2CAemuNE3KDKoD.2HqTA9nMjnJj3PnQexFO7Y/7hqeJhuK', 'Robert', 'Golden', 'rg@gmail.com');
+
+CREATE TABLE `course_registration`.`User_Student` (`Username` VARCHAR(10) NOT NULL, `SID` INT NOT NULL, `DID` TEXT NOT NULL, `Degree` TEXT NOT NULL, PRIMARY KEY (`Username`));
+INSERT INTO `User_Student` (`Username`, `SID`, `DID`, `Degree`) VALUES ('jxw123', '1', '1', 'graduate');
+INSERT INTO `User_Student` (`Username`, `SID`, `DID`, `Degree`) VALUES ('rxg456', '2', '1', 'undergraduate');
+INSERT INTO `User_Student` (`Username`, `SID`, `DID`, `Degree`) VALUES ('mxh789', '3', '2', 'graduate');
+
+CREATE TABLE `course_registration`.`term` (`Semester` VARCHAR(6) NOT NULL, `Year` VARCHAR(4) NOT NULL, `currentTermToRegister` BOOLEAN NOT NULL, PRIMARY KEY(`Semester`, `Year`));
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Fall", 2019, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Spring", 2019, "1");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Summer", 2019, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Fall", 2020, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Spring", 2020, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Summer", 2020, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Fall", 2018, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Spring", 2018, "0");
+INSERT INTO `term` (`Semester`, `Year`, `currentTermToRegister`) VALUES ("Summer", 2018, "0");
+
+CREATE TABLE `course_registration`.`textbook` (`CID` VARCHAR(13) NOT NULL, `Src` VARCHAR(10) NOT NULL, PRIMARY KEY(`CID`));
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('1','1.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('2','2.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('3','3.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('4','4.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('5','5.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('6','6.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('7','7.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('8','None');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('9','9.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('10','10.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('11','11.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('12','None');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('13','13.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('14','14.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('15','15.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('16','16.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('17','17.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('18','None');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('19','19.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('20','20.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('21','21.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('22','22.jpg');
+INSERT INTO `textbook` (`CID`, `Src`) VALUES ('23','23.jpg');
